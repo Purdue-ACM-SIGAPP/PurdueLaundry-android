ee21da19e6bf91192a0fa5a1b15ab58b039c2475
hughesjeff <MacApple12345>
2015-10-02 04:58:58 -0400

    Implemented a way of refreshing the list courtesy of SwipeRefreshLayout

--- app/src/main/java/xyz/jhughes/laundry/LaundryMainActivity.java
+++ app/src/main/java/xyz/jhughes/laundry/LaundryMainActivity.java
@@ -3,15 +3,11 @@ package xyz.jhughes.laundry;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
+import android.widget.*;
 import xyz.jhughes.laundry.LaundryParser.Constants;
 import xyz.jhughes.laundry.LaundryParser.LaundryGetter;
 import xyz.jhughes.laundry.LaundryParser.Machine;
@@ -23,10 +19,11 @@ import java.util.ArrayList;
  * Created by hughesjeff
  */
 
-public class LaundryMainActivity extends AppCompatActivity {
+public class LaundryMainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     private ArrayList<Machine> classMachines;
     private ListView lv;
+    private Spinner s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +38,23 @@ public class LaundryMainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Constants.getListOfRooms());
+        setUpSpinner();
 
-        Spinner s = (Spinner) findViewById(R.id.spinner);
+        GetMachineInfoAsyncTask task = new GetMachineInfoAsyncTask();
+        try {
+            task.execute(Constants.getName(null));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ((SwipeRefreshLayout) findViewById(R.id.swipe_layout)).setOnRefreshListener(this);
+    }
+
+    public void setUpSpinner() {
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Constants.getListOfRooms());
+
+        s = (Spinner) findViewById(R.id.spinner);
         s.setAdapter(adapter);
 
         s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -62,29 +73,17 @@ public class LaundryMainActivity extends AppCompatActivity {
                 System.out.println("Testing");
             }
         });
-
-        GetMachineInfoAsyncTask task = new GetMachineInfoAsyncTask();
-        try {
-            task.execute(Constants.getName(null));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_laundry_main, menu);
-        return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+    public void onRefresh() {
+        refreshList();
+        ((SwipeRefreshLayout) findViewById(R.id.swipe_layout)).setRefreshing(false);
+    }
 
-        //TODO add refresh button
-
-        return super.onOptionsItemSelected(item);
+    public void refreshList() {
+        System.out.println("NYI");
+        Toast.makeText(this, "Not Yet Implemented", Toast.LENGTH_LONG).show();
     }
 
     /**
