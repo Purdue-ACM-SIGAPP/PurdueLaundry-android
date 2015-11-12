83bed5cee908dd64fec1e963ba2192ee8865609c
Tylor Garrett <tylorgarrett@gmail.com>
2015-11-12 09:12:39 -0500

    Updated main activity with pitlor merge

--- app/src/main/java/xyz/jhughes/laundry/MainActivity.java
+++ app/src/main/java/xyz/jhughes/laundry/MainActivity.java
@@ -1,6 +1,7 @@
 package xyz.jhughes.laundry;
 
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
@@ -9,15 +10,48 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import xyz.jhughes.laundry.FragmentPagerAdapter.AppSectionsPagerAdapter;
+import xyz.jhughes.laundry.MachineFragments.MachineFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Managing our tabs
+     */
+    private ViewPager viewPager;
+
+    /**
+     * The layout we use for the drawer, which is simply a ListView in our case.
+     */
+    private DrawerLayout mDrawerLayout;
+
+    /**
+     * Managing whether it'currentRoom open or closed
+     */
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    /**
+     * The list of rooms available
+     */
+    private ListView mDrawerList;
+
+    /**
+     * The current room
+     */
     private static String currentRoom;
+
+    private AppSectionsPagerAdapter appSectionsPagerAdapter;
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +71,100 @@ public class MainActivity extends AppCompatActivity {
         }
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        AppSectionsPagerAdapter appSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager(), currentRoom);
+        appSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager(), currentRoom);
         viewPager.setAdapter(appSectionsPagerAdapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public void createDialog() {
+        final boolean[] tempOptions = transformOptions(MachineFragment.options);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Set the dialog title
+        builder.setTitle(R.string.select_options)
+                // Specify the list array, the items to be selected by default (null for none),
+                // and the listener through which to receive callbacks when items are selected
+                .setMultiChoiceItems(R.array.options, tempOptions,
+                        new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which,
+                                                boolean isChecked) {
+                                tempOptions[which] = isChecked;
+                            }
+                        })
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        String options = transformOptions(tempOptions);
+                        MachineFragment.options = options;
+                        appSectionsPagerAdapter.notifyDataSetChanged();
+
+                        SharedPreferences.Editor e = getSharedPreferences("xyz.jhughes.laundry", MODE_PRIVATE).edit();
+                        e.putString("options", options);
+                        e.apply();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Do nothing
+                            }
+                        });
+        builder.create().show();
+    }
+
+    private String transformOptions(boolean[] options) {
+        String result = "";
+        boolean hasFirst = false;
+
+        if (options[0]) {
+            result += "Available";
+            hasFirst = true;
+        }
+
+        if (options[1]) {
+            result += hasFirst ? "|In use" : "In use";
+            hasFirst = true;
+        }
+
+        if (options[2]) {
+            result += hasFirst ? "|Almost done" : "Almost done";
+            hasFirst = true;
+        }
+
+        if (options[3]) {
+            result += hasFirst ? "|End of cycle" : "End of cycle";
+        }
+
+        return result;
+    }
+
+    private boolean[] transformOptions(String options) {
+        return new boolean[]{
+                options.contains("Available"),
+                options.contains("In use"),
+                options.contains("Almost done"),
+                options.contains("End of cycle")
+        };
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.display_parameters:
+                createDialog();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public static String getSelected() {
