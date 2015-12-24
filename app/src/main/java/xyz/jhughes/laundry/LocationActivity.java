61b1b7baa4e3d50de5e5ddc57b2f4ce933f58264
Kyle <kyle@kptechblog.com>
2015-12-24 17:07:19 -0600

    remove debug log

--- app/src/main/java/xyz/jhughes/laundry/LocationActivity.java
+++ app/src/main/java/xyz/jhughes/laundry/LocationActivity.java
@@ -59,7 +59,6 @@ public class LocationActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.location_activity_toolbar);
         setSupportActionBar(toolbar);
-        Log.d("DEBUG=", String.valueOf(BuildConfig.DEBUG));
 
         try {
             // Obtain the shared Tracker instance.
