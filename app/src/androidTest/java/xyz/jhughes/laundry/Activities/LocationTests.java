01017b79f25f801056dd703f37fd8f98fcd25d6c
vieck <vieck@purdue.edu>
2017-03-28 19:26:26 -0400

    Start of machine activity tests

--- app/src/androidTest/java/xyz/jhughes/laundry/Activities/LocationTests.java
+++ app/src/androidTest/java/xyz/jhughes/laundry/Activities/LocationTests.java
@@ -13,12 +13,10 @@ import android.widget.TextView;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import xyz.jhughes.laundry.LaundryParser.Constants;
 import xyz.jhughes.laundry.LaundryParser.Rooms;
 import xyz.jhughes.laundry.Matchers.RecyclerViewItemCountAssertion;
 import xyz.jhughes.laundry.Matchers.RecyclerViewMatcher;
@@ -26,8 +24,8 @@ import xyz.jhughes.laundry.R;
 import xyz.jhughes.laundry.activities.LocationActivity;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
@@ -103,5 +101,12 @@ public class LocationTests {
         recyclerViewMatcher.atPositionOnView(0,R.id.text_view_dryer).matches(isDisplayed());
         recyclerViewMatcher.atPositionOnView(0,R.id.text_view_dryer_count).matches(isDisplayed());
         recyclerViewMatcher.atPositionOnView(0,R.id.text_view_dryer_total).matches(isDisplayed());
+    }
+
+    @Test
+    public void verifyLaunchMachineActivity() {
+        checkLocationActivity();
+        RecyclerViewMatcher recyclerViewMatcher = withRecyclerView(R.id.recycler_view);
+        onView(recyclerViewMatcher.atPosition(0)).perform(click());
     }
 }
