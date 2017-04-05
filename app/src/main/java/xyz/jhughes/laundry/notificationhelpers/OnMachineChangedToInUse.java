f653411cc82d3887a4af07b8f7eb5450b8f74e1b
pizzamath <slangows@purdue.edu>
2017-04-04 21:11:31 -0400

    Modified listener to listen for timeout

--- app/src/main/java/xyz/jhughes/laundry/notificationhelpers/OnMachineChangedToInUse.java
+++ app/src/main/java/xyz/jhughes/laundry/notificationhelpers/OnMachineChangedToInUse.java
@@ -6,6 +6,7 @@ import xyz.jhughes.laundry.LaundryParser.Machine;
  * Created by Slang on 3/2/2017.
  */
 
-public interface OnMachineInUse {
-    public void onMachineInUse(Machine m);
+public interface OnMachineChangedToInUse {
+    void onMachineInUse(Machine m);
+    void onTimeout();
 }
